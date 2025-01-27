6fc207e016754d66e8e9436f645601ddb394e670
Umer Farooq <umer_farooq@ninjascode.com>
2025-01-27 12:05:10 +0500

    added tailwind css

--- app/(drawer)/(tabs)/discovery/index.tsx
+++ app/(drawer)/(tabs)/discovery/index.tsx
@@ -4,7 +4,7 @@ export default function HomeScreen() {
   return (
     <View className="flex-1 justify-center items-center bg-black">
       <TouchableOpacity className="bg-teal-500 p-3 rounded-lg">
-        <Text className="text-white text-3xl font-bold">Settings</Text>
+        <Text className="text-white text-3xl font-bold">Settingssss</Text>
       </TouchableOpacity>
 
     </View>
