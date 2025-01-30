99c54a39b96a8fb23b39976cbdf03c750c33cfef
Umer Farooq <umer_farooq@ninjascode.com>
2025-01-30 12:13:07 +0500

    added sound context

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,6 @@ import { Redirect } from "expo-router";
 import React from "react";
 
 export default function App() {
-  return <Redirect href="/(tabs)/home/orders" />;
-  // return <Redirect href="/login" />;
+  // return <Redirect href="/(tabs)/home/orders" />;
+  return <Redirect href="/login" />;
 }
